36c479a990eacfe364cd13d89888f87100c53e01
Jdowdy <Jdowdy9k@gamil.com>
2020-06-18 16:34:43 -0400

    working on routes

--- server.js
+++ server.js
@@ -1,66 +1,45 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+var path = require("path");
 
 const PORT = process.env.PORT || 3030;
-
-const User = require("./models/workout");
 const app = express();
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+const Workout = require("./models/workout");
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
 
-//script to initially seed database
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 
- //const { Seeder } = require('mongo-seeding')
- //const config = {
- //  database: {
- //    name: 'workout',
- //  },
- //  dropDatabase: true,
- //};
- //const seeder = new Seeder(config);
- //const collections = seeder.readCollectionsFromPath(
- //  path.resolve("\seeders"),
- //  {
- //    transformers: [Seeder.Transformers.replaceDocumentIdWithUnderscoreId],
- //  },
- //);
-//
-// seeder
-   .import(collections)
-   .then(() => {
-     console.log('Success');
-   })
-   .catch(err => {
-     console.log('Error', err);
-   });
 
 // routes
-app.use(require("./routes/routes.js"));
+//app.use(require("./routes/routes.js"));
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/exercise", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/excercise.html"));
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
 });
 
 app.get("/exercise?", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/excercise.html"));
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
 });
 
 app.get("/stats", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+
+app.listen(PORT,function(){ 
+    console.log(`App listening on Port ${PORT}`);
 });
