852a6cd94af32027802c95222590a53ba05aac25
Jdowdy <Jdowdy9k@gamil.com>
2020-06-22 20:37:09 -0400

    working on mongo and heroku

--- server.js
+++ server.js
@@ -14,7 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds221242.mlab.com:21242/heroku_fph29cbd", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds221242.mlab.com:21242/heroku_fph29cbd", {
+     useNewUrlParser: true,
+     useFindAndModify: false
+}).then(function(){
+    return console.log("connected to mongo")
+},function(err){
+    console.log(err)
+})
 
 app.use(require("./routes/routes.js"));
 
